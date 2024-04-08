cb6776773a1234d139df9188341c88223ca1f06a
David Bomba <turbo124@gmail.com>
2024-04-08 16:23:48 +1000

    Updated tests

--- src/FatturaPA/common/CessionarioCommittente.php
+++ src/FatturaPA/common/CessionarioCommittente.php
@@ -2,10 +2,38 @@
 
 namespace CleverIt\UBL\Invoice\FatturaPA\common;
 
+use Sabre\Xml\Writer;
+use Sabre\Xml\XmlSerializable;
+use CleverIt\UBL\Invoice\BaseInvoice;
 
-class CessionarioCommittente
+class CessionarioCommittente extends BaseInvoice implements XmlSerializable
 {
-    public $DatiAnagrafici; //DatiAnagrafici
-    public $Sede; //Sede
+    private array $protected = [
+        'protected',
+    ];
+
+    public function __construct(public DatiAnagrafici $DatiAnagrafici, public Sede $Sede){}
+
+    function xmlSerialize(Writer $writer): void
+    {
+
+        $vars = [];
+
+        foreach($this as $property => $value) {
+
+            if(in_array($property, $this->protected)) {
+                continue;
+            }
+
+            if($value !== null) {
+                $vars[$property] = $value;
+            }
+        }
+
+        $this->setProps($vars);
+
+        $writer->write($this->getProps());
+
+    }
 
 }
