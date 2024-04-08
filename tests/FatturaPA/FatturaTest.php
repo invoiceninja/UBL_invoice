020daff216157b44c53d8663ebdc57136e23087a
David Bomba <turbo124@gmail.com>
2024-04-08 13:40:35 +1000

    Tests for fattura

--- tests/FatturaPA/FatturaTest.php
+++ tests/FatturaPA/FatturaTest.php
@@ -2,8 +2,10 @@
 
 namespace CleverIt\UBL\Invoice\Tests\FatturaPA;
 
-use CleverIt\UBL\Invoice\FatturaPA\common\FatturaElettronicaBody;
+use Generator;
+use Sabre\Xml\Service;
 use PHPUnit\Framework\TestCase;
+use CleverIt\UBL\Invoice\FatturaPA\common\FatturaElettronicaBody;
 
 class FatturaTest extends TestCase
 {
@@ -26,6 +28,33 @@ class FatturaTest extends TestCase
 
         $this->assertEquals($f->getDatiGenerali(), "2024-01-01");
 
+    }
+
+    public function testXmlBuild()
+    {
+        
+        $f = new FatturaElettronicaBody();
+        $f->setDatiGenerali("2024-01-01");
+
+        $root_element = 'FatturaElettronicaHeader';
+
+        $xmlService = new Service();
+
+        $xmlService->namespaceMap = [
+            'p:FatturaElettronica' => '',
+            'versione' => 'FPA12',
+            //'xmlns:ds' => 'http://www.w3.org/2000/09/xmldsig#',
+            //'xmlns:p' => 'http://ivaservizi.agenziaentrate.gov.it/docs/xsd/fatture/v1.2',
+            //'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+            //'xsi:schemaLocation' => 'http://ivaservizi.agenziaentrate.gov.it/docs/xsd/fatture/v1.2 http://www.fatturapa.gov.it/export/fatturazione/sdi/fatturapa/v1.2/Schema_del_file_xml_FatturaPA_versione_1.2.xsd'
+        ];
+
+        
+        $xml = $xmlService->write($root_element, $f);
+   
+        echo $xml;
+
+        $this->assertIsString($xml);
     }
 
 }
