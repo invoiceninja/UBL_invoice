8d5b7410c919a77d7287cba44254d7990fee82b4
David Bomba <turbo124@gmail.com>
2024-05-08 16:55:02 +1000

    Fixes for fact extract

--- src/Command/RooCommand.php
+++ src/Command/RooCommand.php
@@ -41,6 +41,8 @@ final class RooCommand extends Command
            "min_length" => null,
            "pattern" => null,
            "whitespace" => null,
+           "minOccurs" => null,
+           "maxOccurs" => null,
    ];
 
     protected \DomDocument $document;
@@ -102,12 +104,6 @@ final class RooCommand extends Command
 
         $this->getParentTypes();
 
-        // $this->extractTypes();
-
-        // $child_types = $this->extractChildTypes();
-
-        // $data = $this->normalizeTypes($data);
-
         $this->output->writeln([" >> Writing sequence_elements to file",'============',]);
         $elementsString = json_encode($this->data, JSON_PRETTY_PRINT);
         $fp = fopen("./stubs/FactRoo.json", 'w');
@@ -116,142 +112,6 @@ final class RooCommand extends Command
 
         return self::SUCCESS;
 
-    }
-
-    private function searchForResource(string $name): array
-    {
-
-        $match = [];
-
-        match($name) {
-            'UBLExtension' => $match = ['ext', 'UBLExtension'],
-            'UBLExtensionsType' => $match = ['ext', 'UBLExtensions'],
-            default => $match = [],
-
-        };
-
-        return $match;
-    }
-
-    private function loadResource(string $type): self
-    {
-        $parts = explode(":", $type);
-
-        if(count($parts) == 1) {
-            $parts = $this->searchForResource($type);
-        }
-
-        $path = $this->resource_map["{$parts[0]}:"];
-
-        $this->document->load($path);
-
-        return $this;
-    }
-
-    public function extractChildTypes(): self
-    {
-
-        $child_array = [];
-
-        foreach($this->data as $key => $types) {
-
-            foreach($types['elements'] as $eKey => $element) {
-
-                $this->document->load($this->resource);
-
-                $base_type = $element['base_type'];
-                $nodes = $this->getElementPath($element['name'], "./{$this->prefix}:complexType [@name='{$base_type}']");
-
-                $sequence = $this->extractSequence($nodes->item(0));
-                $sequence_list = $this->processSequences($sequence);
-                $child_array['type'] = $base_type;
-                $child_array['elements'] = $sequence_list;
-
-                $this->data[] = $child_array;
-
-                $child_array = [];
-            }
-
-        }
-
-
-        return $this;
-    }
-    public function getType(string $type): ?string
-    {
-
-        $this->loadResource($type);
-
-        $parts = explode(":", $type);
-
-        if(count($parts) == 1) {
-            $parts = $this->searchForResource($type);
-        }
-
-        $name = $parts[1];
-
-        $element = $this->getElement($name);
-
-        return $element->getAttribute("type");
-
-    }
-
-    public function loadDoc(string $type): self
-    {
-
-        $this->loadResource($type);
-
-        return $this;
-    }
-    public function getElementPath(string $name, string $path, ?DOMElement $element = null): ?\DOMNodeList
-    {
-        $this->loadDoc($name);
-
-        $xpath = new \DOMXPath($this->document);
-        $elements = $xpath->query($path, $element);
-
-        return $elements->count() > 0 ? $elements : null;
-
-    }
-
-    public function extractHelp(string $type): ?string
-    {
-
-        $this->loadResource($type);
-
-        $parts = explode(":", $type);
-        $name = $parts[1];
-
-        $element = $this->getElement($name);
-
-        return $this->extractAnnotation($element);
-    }
-
-    public function getElement($name): ?\DOMElement
-    {
-
-        $xpath = new \DOMXPath($this->document);
-        $elements = $xpath->query("./{$this->prefix}:element [@name='{$name}']");
-
-        if($elements->count() == 0 || !($elements->item(0) instanceof \DOMElement)) {
-            throw new \Exception("Could not find element for type {$name}");
-        }
-
-        return $elements->item(0);
-    }
-
-
-    public function extractTypes(): self
-    {
-        foreach($this->data as $key => $value) {
-
-            foreach($value['elements'] as $cKey => $element) {
-                $this->data[$key]['elements'][$cKey]['base_type'] = $this->getType($element['name']);
-                $this->data[$key]['elements'][$cKey]['help'] = $this->extractHelp($element['name']);
-            }
-        }
-
-        return $this;
     }
 
     public function getParentTypes(): self
@@ -293,26 +153,22 @@ final class RooCommand extends Command
 
             foreach($node->childNodes as $childNode) {
 
+                $child_array = [];
                 if($childNode instanceof DOMElement) {
                     
                     $name = $childNode->getAttribute('ref');
+
+                    if($name == 'ext:UBLExtensions')
+                        continue;
                     
-                    $tmp[] = $this->harvestNode($name);
+                    $child_array = $this->harvestNode($name);
+                    $child_array['minOccurs'] = $childNode->getAttribute('minOccurs') ?? "0";
+                    $child_array['maxOccurs'] = $childNode->getAttribute('maxOccurs') ?? "1";
 
-                    // foreach($childNode->attributes as $attr)
-                    // {
-                        
-                        
-                    // }
-
-                    // $child_array = $this->extractAttributes($childNode);
-                    // if(count($child_array) > 0) {
-
-                    //     $child_array['base_type'] = $this->type->hydrateTypes($child_array['name']);
-                    //     $tmp[] = array_merge($this->stub_validation, $child_array);
-
-                    // }
-
+                    if($child_array['maxOccurs'] == 'unbounded')
+                        $child_array['maxOccurs'] = "-1";
+                    
+                    $tmp[] = $child_array;
                 }
 
             }
@@ -350,42 +206,6 @@ final class RooCommand extends Command
         return $type->getPrimativeType($base_type);
 
         return $base_type;
-
-    }
-
-    public function harvestTypeArray(string $name, string $type): array
-    {
-        $this->loadResource($name);
-
-        $content = [];
-
-        $node = $this->getElementPath($type, "./{$this->prefix}:complexType//{$this->prefix}:simpleContent//{$this->prefix}:extension");
-
-        if($node->count() == 1) {
-            $element_type = $node->item(0)->getAttribute("base");
-            $root_type = $this->getType($element_type);
-            //echo "root". PHP_EOL;
-            //echo $root_type.PHP_EOL;
-        }
-
-        return [];
-    }
-
-    public function extractAttributes(\DomElement $childNode): array
-    {
-        $child_array = [];
-
-        foreach($childNode->attributes as $key => $attr) {
-
-            if(in_array($attr->nodeName, ['ref', 'minOccurs','maxOccurs'])) {
-
-                $key = $attr->nodeName == 'ref' ? 'name' : $attr->nodeName;
-
-                $child_array[$key] = $attr->nodeValue;
-            }
-        }
-
-        return $child_array;
 
     }
 
